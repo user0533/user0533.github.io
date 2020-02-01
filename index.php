346218f55e1a7517f50c1b027dc0b38e81364866
user0533 <60549189+user0533@users.noreply.github.com>
2020-02-01 21:42:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,98 +1,33 @@
 <?php
-if (!isset($_REQUEST)) {
-    return;
+$confirmation_token = 'CONF_TOKEN'
+function vk_msg_send($peer_id,$text){
+	$request_params = array(
+		'message' => $text, 
+		'peer_id' => $peer_id, 
+		'access_token' => "e437e5df6a17028839f7a949eceaed3aa59c31205d5f2b7adcfc6e0bd6455a0083dd0b0985f048f397f88",
+		'v' => '5.87' 
+	);
+	$get_params = http_build_query($request_params); 
+	file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
 }
-
-//Строка для подтверждения адреса сервера из настроек Callback API
-$confirmationToken = '7981442e';
-
-//Ключ доступа сообщества
-$token = '5d20587bc33c19bfaed84834fedaa060406a171824108a6b8474da3d60a56f97373dfde9f10541f2ac133';
-
-// Secret key
-$secretKey = 'gg';
-
-//Получаем и декодируем уведомление
+$arr = file('https://evkappe.herokuapp.com/dict.txt');
 $data = json_decode(file_get_contents('php://input'));
-
-// проверяем secretKey
-if(strcmp($data->secret, $secretKey) !== 0 && strcmp($data->type, 'confirmation') !== 0)
-    return;
-
-//Проверяем, что находится в поле "type"
-switch ($data->type) {
-    //Если это уведомление для подтверждения адреса сервера...
-    case 'confirmation':
-        //...отправляем строку для подтверждения адреса
-        echo $confirmationToken;
-        break;
-
-    //Если это уведомление о новом сообщении...
-    case 'message_new':
-        //...получаем id его автора
-        $userId = $data->object->user_id;
-        //затем с помощью users.get получаем данные об авторе
-        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&v=5.0"));
-
-        //и извлекаем из ответа его имя
-        $text = $data->object->body;
-
-        //С помощью messages.send и токена сообщества отправляем ответное сообщение
-        if ($text=='1') {
-            $request_params = array(
-            'message' => "1",
-            'user_id' => $userId,
-            'access_token' => $token,
-            'v' => '5.0'
-        );
+switch ($data->type) {  
+	case 'confirmation': 
+		echo $confirmation_token; 
+	break;  
+		
+	case 'message_new': 
+		$message_text = $data -> object -> text;
+		$chat_id = $data -> object -> peer_id;
+        $message_text = explode(" ",$message_text);
+        if($message_text[2]<=count($arr) && $message_text[2]>=1){
+            vk_msg_send($chat_id,$arr[$message_text[2]-1]);
         }
-        else {
-                   $request_params = array(
-            'message' => "2",
-            'user_id' => $userId,
-            'access_token' => $token,
-            'v' => '5.0' 
-                       );
+        else{
+             vk_msg_send($chat_id,"Idont understand");
         }
-
-        $get_params = http_build_query($request_params);
-
-        file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
-
-        //Возвращаем "ok" серверу Callback API
-        echo('ok');
-
-        break;
-
-    // Если это уведомление о вступлении в группу
-    case 'group_join':
-        //...получаем id нового участника
-        $userId = $data->object->user_id;
-
-        //затем с помощью users.get получаем данные об авторе
-        $userInfo = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$userId}&v=5.0"));
-
-        //и извлекаем из ответа его имя
-        $user_name = $userInfo->response[0]->first_name;
-
-        //С помощью messages.send и токена сообщества отправляем ответное сообщение
-        $request_params = array(
-            'message' => "Добро пожаловать в наше сообщество МГТУ им. Баумана ИУ5 2016, {$user_name}!<br>" .
-                            "Если у Вас возникнут вопросы, то вы всегда можете обратиться к администраторам сообщества.<br>" .
-                            "Их контакты можно найти в соответсвующем разделе группы.<br>" .
-                            "Успехов в учёбе!",
-            'user_id' => $userId,
-            'access_token' => $token,
-            'v' => '5.0'
-        );
-
-        $get_params = http_build_query($request_params);
-
-        file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
-
-        //Возвращаем "ok" серверу Callback API
-        echo('ok');
-
-        break;
+		echo 'ok';
+	break;
 }
 ?>
